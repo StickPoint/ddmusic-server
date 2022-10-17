2930f47c714bf322dc4b8357843d448cf8be273b
0303 <suncx@flamelephant.com>
2022-10-17 11:56:04 +0800

    ddmusic-server 001 init project
    初始化顶点音乐后台服务

---
+++ src/main/java/com/ddmusic/ddmusicserver/DdmusicServerApplication.java
@@ -0,0 +1,13 @@
+package com.ddmusic.ddmusicserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdmusicServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DdmusicServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ddmusic/ddmusicserver/DdmusicServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddmusic.ddmusicserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DdmusicServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
